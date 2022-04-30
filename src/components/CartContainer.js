1653e50efddbc155828eb7e233059ee6bfd6956b
Jignesh Patel <jignesh@foodme.mobi>
2022-04-30 16:32:34 +0530

    first commit

---
+++ src/components/CartContainer.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import CartItem from './CartItem';
+import { CLEAR_CART, GET_TOTALS } from '../actions';
+
+const CartContainer = () => {
+  const { cart, total } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({ type: GET_TOTALS });
+  }, [cart]);
+  if (cart.length === 0) {
+    return (
+      <section className='cart'>
+        {/* cart header */}
+        <header>
+          <h2>your bag</h2>
+          <h4 className='empty-cart'>is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+  return (
+    <section className='cart'>
+      {/* cart header */}
+      <header>
+        <h2>your bag</h2>
+      </header>
+      {/* cart items */}
+      <article>
+        {cart.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </article>
+      {/* cart footer */}
+      <footer>
+        <hr />
+        <div className='cart-total'>
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button
+          className='btn clear-btn'
+          onClick={() => dispatch({ type: CLEAR_CART })}
+        >
+          clear cart
+        </button>
+      </footer>
+    </section>
+  );
+};
+
+export default CartContainer;
